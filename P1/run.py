9ede9e56947cbfacfe386262b065703744ed846e
larsmhod <larsmhod@ntnu.no>
2021-01-11 12:54:18 +0100

    cmd line args

--- P1/run.py
+++ P1/run.py
@@ -1,6 +1,22 @@
+import getopt
 import sys
 
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+    short_options = "hc:o"
+    long_options = ["help", "config", "output"]
+
+    full_cmd_arguments = sys.argv
+    argument_list = full_cmd_arguments[1:]
+
+    try:
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    except getopt.error as err:
+        # Output error, and return with an error code
+        print(str(err))
+        sys.exit(2)
+
+    for current_argument, current_value in arguments:
+        elif current_argument in ("-h", "--help"):
+            print("Required args: \n Optional args:")
+        elif current_argument in ("-o", "--output"):
+            print(("Outputtin to (%s)") % (current_value))
