19e66dfc62dadfa3c37d9c06421c19dc6b0e75e0
LM239 <32392200+LM239@users.noreply.github.com>
2021-01-12 09:37:05 +0100

    Create pegsol_world.py

---
+++ P1/pegsol_world.py
@@ -0,0 +1,13 @@
+
+class Peg_solitaire:
+
+    def __init__(self, config):
+      if (config.type == "triangle"):
+          self.state = [[1] * i for i in range(1, config.size + 1)]
+      elif (config.type == "diamond"):
+          self.state = [[1] * config.size] * config.size
+      else:
+          print("Unknown board type {}. Exiting".format(config.type))
+          exit(1)
+      
+      return 0
