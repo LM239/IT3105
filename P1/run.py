f97b7203eeb470550bcc25d30de8030d934df575
larsm <larsmartin239@gmail.com>
2021-01-12 21:25:08 +0100

    tidied code

--- P1/run.py
+++ P1/run.py
@@ -6,21 +6,18 @@ from worlds.pegsol_world import PegSolitaire
 if __name__ == "__main__":
     short_options = "hc:"
     long_options = ["help", "config"]
-
-    full_cmd_arguments = sys.argv
-    argument_list = full_cmd_arguments[1:]  # remove filename
+    argument_list = sys.argv[1:]  # remove filename
 
     try:
         arguments, values = getopt.getopt(argument_list, short_options, long_options)
     except getopt.error as err:
-        # Output error, and return with an error code
         print(str(err))
         sys.exit(2)
 
     path = ""
     for current_argument, current_value in arguments:
         if current_argument in ("-h", "--help"):
-            print("Required args: \n Optional args:")
+            print("Required args: -c <path-to-config> \n Optional args: none")
         elif current_argument in ("-c", "--config"):
             path = current_value
 
@@ -29,9 +26,8 @@ if __name__ == "__main__":
             configs = yaml.load(file, Loader=yaml.FullLoader)
             file.close()
     except FileNotFoundError:
-       print("Could not open file at {}".format(path))
-       print("Exiting")
-       exit(1)
+        print("Could not open file at {}\nExiting".format(path))
+        exit(1)
 
     actor_config = configs["actor"]
     critic_config = configs["critic"]
