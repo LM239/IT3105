f7a7e2b0520545e453ef782778c599bfd9540f23
larsm <larsmartin239@gmail.com>
2021-01-12 11:55:34 +0100

    implemented cmdline config argument

--- P1/run.py
+++ P1/run.py
@@ -1,5 +1,7 @@
 import getopt
 import sys
+import yaml
+from .worlds.pegsol_world import PegSolitaire
 
 if __name__ == "__main__":
     short_options = "hc:"
@@ -15,10 +17,29 @@ if __name__ == "__main__":
         print(str(err))
         sys.exit(2)
 
+    path = ""
     for current_argument, current_value in arguments:
         if current_argument in ("-h", "--help"):
             print("Required args: \n Optional args:")
         elif current_argument in ("-c", "--config"):
-            continue
+            path = current_value
+
+    try:
+        with open(path) as file:
+            configs = yaml.load(file, Loader=yaml.FullLoader)
+            file.close()
+    except FileNotFoundError:
+       print("Could not open file at {}".format(path))
+       print("Exiting")
+       exit(1)
+
+    actor_config = configs["actor"]
+    critic_config = configs["critic"]
+    world_config = configs["sim_world"]
+
+    world = PegSolitaire(world_config)
+    print(actor_config)
+    print(critic_config)
+    print(world_config)
 
     exit(0)
--- P1/worlds/pegsol_world.py
+++ P1/worlds/pegsol_world.py
@@ -1,21 +1,19 @@
 
-class Peg_solitaire:
+class PegSolitaire:
 
     def __init__(self, config):
-        if (config.type == "triangle"):
+        if config.type == "triangle":
             self.state = [[1] * i for i in range(1, config.size + 1)]
-        elif (config.type == "diamond"):
+        elif config.type == "diamond":
             self.state = [[1] * config.size] * config.size
         else:
             print("Unknown board type {}. Exiting".format(config.type))
             exit(1)
-      
-        return self
-    
     
     def __str__(self):
-        return "010101010"
+        val = ""
+        return val.join(str(peg) for row in self.state for peg in row)
     
     def vector(self):
-        return [peg for peg in row for row in self.state]
-    
+        return [peg for row in self.state for peg in row]
+
