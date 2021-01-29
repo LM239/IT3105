4bfaf40624d08e86051d9ddc8b5e26c225dfb0ee
larsmhod <larsmhod@ntnu.no>
2021-01-29 10:34:15 +0100

    new update rule, remove split_gd

--- P1/neural_critic.py
+++ P1/neural_critic.py
@@ -2,10 +2,11 @@ from keras.optimizers import SGD
 from keras.models import Sequential
 from keras.layers import Dense, Input
 from keras.losses import Reduction
-from splitgd import SplitGD
 from configs.validate_configs import validate_critic_config
 from keras.losses import MeanSquaredError
+import tensorflow as tf
 import numpy as np
+
 
 class NeuralCritic:
 
@@ -17,26 +18,54 @@ class NeuralCritic:
         self.eligibility_decay = critic_cfg["eligibility_decay"]
         self.discount_factor = critic_cfg["discount_factor"]
 
-        model = Sequential()
-        model.add(Input(shape=(input_dim,)))
+        self.model = Sequential()
+        self.model.add(Input(shape=(input_dim,)))
         for layer in self.size:
-            model.add(Dense(layer, activation='tanh', use_bias=False))
-        model.add(Dense(1, use_bias=False))
-        model.compile(optimizer=SGD(learning_rate=self.lr), loss=MeanSquaredError(reduction=Reduction.NONE))
+            self.model.add(Dense(layer, activation='tanh', use_bias=False))
+        self.model.add(Dense(1, use_bias=False))
+        self.model.compile(optimizer=SGD(learning_rate=self.lr), loss=MeanSquaredError(reduction=Reduction.NONE))
 
-        self.split_gd = SplitGD(model)
+        self.eligibilities = [np.zeros(shape=(layer.input_shape[1], layer.output_shape[1])) for layer in
+                              self.model.layers]
         self.episode = []
 
     def update(self, episode, state, state_prime, reward):
-        target = reward + self.discount_factor * self.split_gd.model.predict([state_prime], batch_size=1)
-        delta = target - self.split_gd.model.predict([state], batch_size=1)
+        target = reward + self.discount_factor * self.model.predict([state_prime], batch_size=1)
+        delta = target - self.model.predict([state], batch_size=1)
         self.episode.append((np.array([state]), target, delta))
         return delta
 
     def finish_episode(self):
+        print("done")
         for state, target, delta in self.episode:
-            self.split_gd.fit([state], [target], delta, vfrac=0.0, verbosity=0)
+            self.fit([state], [target], delta)
         self.episode = []
 
+
     def reset_eligibilities(self):
-        self.split_gd.reset_eligibilities()
+        self.eligibilities = [np.zeros(shape=(layer.input_shape[1], layer.output_shape[1])) for layer in
+                              self.model.layers]
+
+    # Subclass this with something useful.
+    def modify_gradients(self, gradients, delta):
+        for index, el in enumerate(self.eligibilities):
+            self.eligibilities[index] = self.eligibilities[index] * self.eligibility_decay * self.discount_factor + gradients[index] / delta
+            gradients[index] = delta * self.eligibilities[index]
+        return gradients
+
+    # This returns a tensor of losses, OR the value of the averaged tensor.  Note: use .numpy() to get the
+    # value of a tensor.
+    def gen_loss(self, features, targets, avg=False):
+        predictions = self.model(features)  # Feed-forward pass to produce outputs/predictions
+        loss = self.model.loss(targets, predictions)  # model.loss = the loss function
+        return tf.reduce_mean(loss).numpy() if avg else loss
+
+    def fit(self, features, targets, delta, epochs=1):
+        params = self.model.trainable_weights
+        for epoch in range(epochs):
+            with tf.GradientTape() as tape:  # Read up on tf.GradientTape !!
+                loss = self.gen_loss(features, targets, avg=False)
+                gradients = tape.gradient(loss, params)
+                gradients = self.modify_gradients(gradients, delta)
+                self.model.optimizer.apply_gradients(zip(gradients, params))
+
