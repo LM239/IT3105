0dc811d954e3c0046253c1a204b0de736cb4a9fb
larsmhod <larsmhod@ntnu.no>
2021-02-19 13:37:27 +0100

    interface simworld

---
+++ P2/worlds/world.py
@@ -0,0 +1,22 @@
+
+class SimWorld:
+
+    def new_state(self):
+        pass
+
+    def get_actions(self, state):
+        pass
+
+    def do_action(self, state, action):
+        pass
+
+    def in_end_state(self, state) -> bool:
+        pass
+
+    def visualize(self, states):
+        pass
+
+    def vector(self, state):
+        pass
+
+
