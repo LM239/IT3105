24892c07533a2eeff18a5bfaf414857c7631b3bf
larsm <larsmartin239@gmail.com>
2021-02-22 10:16:02 +0100

    småting

--- P2/configs/validate_configs.py
+++ P2/configs/validate_configs.py
@@ -19,16 +19,16 @@ def validate_config(configs):
 
 def validate_hex_board(config):
     if "size" not in config:
-        print("Missing required PegSolitaire argument: 'size' \nExiting")
+        print("Missing required hex argument: 'size' \nExiting")
         exit(1)
     elif config["size"] < 3 or config["size"] > 10:
-        print("Size parameter outside of accepted range: [3,10] \nExiting")
+        print("Size parameter outside of accepted range: [3,) \nExiting")
         exit(1)
 
 
 def validate_mcts(config):
     if "episodes" not in config:
-        print("Missing required actor_config argument: 'episodes' \nExiting")
+        print("Missing required mcts argument: 'episodes' \nExiting")
         exit(1)
     if "search_games" not in config:
         print("Missing required actor_config argument: 'search_games' \nExiting")
@@ -39,25 +39,25 @@ def validate_anet(config):
     valid_activations = ["linear", "sigmoid", "tanh", "relu"]
     valid_optmizers = ["Adagrad", "SGD", "RMSProp", "Adam"]
     if "lr" not in config:
-        print("Missing required Critic argument: 'lr' \nExiting")
+        print("Missing required anet argument: 'lr' \nExiting")
         exit(1)
     if "optimizer" not in config:
-        print("Missing required Critic argument: 'optimizer' \nExiting")
+        print("Missing required anet argument: 'optimizer' \nExiting")
         exit(1)
     if "hidden_layers" not in config:
-        print("Missing required Critic argument: 'hidden_layers' \nExiting")
+        print("Missing required anet argument: 'hidden_layers' \nExiting")
         exit(1)
     if config["optimizer"] not in valid_optmizers:
         print("ANET optimizer invalid. Valid options are:", valid_optmizers, "\nExiting")
     for i, layer in enumerate(config["hidden_layers"]):
         if layer[1] not in valid_activations:
-            print("ANET layer", i, "activation function in invalid. Valid options are:", valid_activations, "\nExiting")
+            print("ANET layer", i, "activation function is invalid. Valid options are:", valid_activations, "\nExiting")
 
 
 def validate_topp(config):
     if "cache_m" not in config:
-        print("Missing required actor_config argument: 'cache_m' \nExiting")
+        print("Missing required topp argument: 'cache_m' \nExiting")
         exit(1)
     if "games_g" not in config:
-        print("Missing required actor_config argument: 'games_g' \nExiting")
+        print("Missing required topp argument: 'games_g' \nExiting")
         exit(1)
