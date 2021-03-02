c1cd398e65f33c6e6bbdd250b6cf5cec947f8097
larsmhod <larsmhod@ntnu.no>
2021-03-02 22:30:13 +0100

    finally working

--- P2/conv_anet.py
+++ P2/conv_anet.py
@@ -1,6 +1,60 @@
+from tensorflow import float32
+
 from configs.validate_configs import validate_anet_config
 from interfaces.actornet import ActorNet
+from keras.models import *
+import tensorflow as tf
+from keras.layers import *
+import os
 
+class ConvNet(ActorNet):
+    def __init__(self, anet_cfg=None, board_size: int = 0, output_dim: int = 0, model_file: str = None):
+        if model_file is None:
+            #validate_anet_config(anet_cfg)
+            self.lr = anet_cfg["lr"]
+            os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+            self.file_structure = anet_cfg["file_structure"]
+
+            #from https://github.com/suragnair/alpha-zero-general/blob/master/gobang/keras/GobangNNet.py
+            self.input_boards = Input(shape=(board_size, board_size, 1))  # s: batch_size x board_x x board_y
+
+            #x_image = Reshape((board_size, board_size, 1))(self.input_boards)  # batch_size  x board_x x board_y x 1
+            h_conv1 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l1_filters"], 3, padding='same')(self.input_boards)))  # batch_size  x board_x x board_y x num_channels
+            h_conv2 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l2_filters"], 3, padding='same')(h_conv1)))  # batch_size  x board_x x board_y x num_channels
+            h_conv3 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l3_filters"], 3, padding='same')(h_conv2)))  # batch_size  x (board_x-2) x (board_y-2) x num_channels
+            h_conv3_flat = Reshape((-1,))(h_conv3)
+            s_fc1 = Dropout(anet_cfg["dropout"])(Activation(anet_cfg["activation"])(BatchNormalization(axis=1)(Dense(512)(h_conv3_flat))))  # batch_size x 1024
+            s_fc2 = Dropout(anet_cfg["dropout"])(Activation(anet_cfg["activation"])(BatchNormalization(axis=1)(Dense(256)(s_fc1))))  # batch_size x 1024
+            self.pi = Dense(output_dim, activation='softmax', name='pi')(s_fc2)  # batch_size x self.action_size
+            #self.v = Dense(1, activation='tanh', name='v')(s_fc2)  # batch_size x 1
+
+            opt = type(tf.keras.optimizers.get(anet_cfg["optimizer"]))(learning_rate=anet_cfg["lr"])
+
+            self.model = Model(inputs=self.input_boards, outputs=self.pi) #, self.v])
+            self.model.compile(optimizer=opt, loss=anet_cfg["loss"])
+        else:
+            self.model = load_model(model_file)
+
+    def train(self, features, targets):
+        self.model.fit(
+            tf.convert_to_tensor(features),  # training data
+            tf.convert_to_tensor(targets),  # training targets
+            epochs=1
+        )
+
+    def forward(self, input):
+        input = tf.convert_to_tensor([input])
+        return self.model(input, training=False)
+
+    def save_params(self, episode: int):
+        file_name = self.file_structure + "checkpoint_" + str(episode) + ".h5"
+        self.model.save(file_name)
+
+
+
+
+
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -69,53 +123,4 @@ class ConvNet(nn.Module):
         torch.save(self, file_name)
 
 """
-from keras.models import Sequential
-from keras.layers import Dense, Input
-from keras.models import *
-import tensorflow as tf
-from keras.layers import *
 
-class ConvNet(ActorNet):
-    def __init__(self, anet_cfg=None, board_size: int = 0, output_dim: int = 0, model_file: str = None):
-        if model_file is None:
-            #validate_anet_config(anet_cfg)
-            self.lr = anet_cfg["lr"]
-
-            self.file_structure = anet_cfg["file_structure"]
-
-            #from https://github.com/suragnair/alpha-zero-general/blob/master/gobang/keras/GobangNNet.py
-            self.input_boards = Input(shape=(board_size, board_size, 1))  # s: batch_size x board_x x board_y
-
-            #x_image = Reshape((board_size, board_size, 1))(self.input_boards)  # batch_size  x board_x x board_y x 1
-            h_conv1 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l1_filters"], 3, padding='same')(self.input_boards)))  # batch_size  x board_x x board_y x num_channels
-            h_conv2 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l2_filters"], 3, padding='same')(h_conv1)))  # batch_size  x board_x x board_y x num_channels
-            h_conv3 = Activation(anet_cfg["activation"])(BatchNormalization(axis=3)(Conv2D(anet_cfg["l3_filters"], 3, padding='valid')(h_conv2)))  # batch_size  x (board_x-2) x (board_y-2) x num_channels
-            h_conv3_flat = Flatten()(h_conv3)
-            s_fc1 = Dropout(anet_cfg["dropout"])(Activation(anet_cfg["activation"])(BatchNormalization(axis=1)(Dense(1024)(h_conv3_flat))))  # batch_size x 1024
-            s_fc2 = Dropout(anet_cfg["dropout"])(Activation(anet_cfg["activation"])(BatchNormalization(axis=1)(Dense(512)(s_fc1))))  # batch_size x 1024
-            self.pi = Dense(output_dim, activation='softmax', name='pi')(s_fc2)  # batch_size x self.action_size
-            #self.v = Dense(1, activation='tanh', name='v')(s_fc2)  # batch_size x 1
-
-            opt = type(tf.keras.optimizers.get(anet_cfg["optimizer"]))(learning_rate=anet_cfg["lr"])
-
-            self.model = Model(inputs=self.input_boards, outputs=[self.pi]) #, self.v])
-            self.model.compile(optimizer=opt, loss=anet_cfg["loss"])
-        else:
-            self.model = load_model(model_file)
-
-    def train(self, features, targets):
-        self.model.fit(
-            features,  # training data
-            targets,  # training targets
-            epochs=1
-        )
-
-    def forward(self, input):
-        print(input.shape)
-        return self.model([input], training=False)
-
-    def save_params(self, episode: int):
-        file_name = self.file_structure + "checkpoint_" + str(episode) + ".h5"
-        self.model.save(file_name)
-
-"""
