59df405525f6f334e080ae2998227ca86c0bedeb
larsmhod <larsmhod@ntnu.no>
2021-03-02 21:17:53 +0100

    better torch

--- P2/conv_anet.py
+++ P2/conv_anet.py
@@ -40,20 +40,29 @@ class ConvNet(nn.Module):
             pass
 
     def forward(self, s):
+        with torch.no_grad():
+            return self.forward_grad(s)
+
+    def forward_grad(self, s):
         #                                                           s: batch_size x board_x x board_y
         s = torch.tensor(s, dtype=torch.float32)
         s = s.view(-1, 1, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
         s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
         s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
-        s = F.relu(self.bn3(self.conv3(s)))                           # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, self.l3_filters*(self.board_x - 2)*(self.board_y - 2))
+        s = F.relu(self.bn3(self.conv3(s)))
+        s = torch.flatten(s, 1)
 
-        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.dropout, training=self.training)  # batch_size x 1024
-        s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.dropout, training=self.training)  # batch_size x 512
+        s = self.fc1(s)
+        if s.shape[0] > 1:
+            s = self.fc_bn1(s)
+        s = F.dropout(F.relu(s), p=self.dropout, training=self.training)  # batch_size x 1024
+        s = self.fc2(s)
+        if s.shape[0] > 1:
+            s = self.fc_bn2(s)
+        s = F.dropout(F.relu(s), p=self.dropout, training=self.training)  # batch_size x 512
 
-        pi = self.fc3(s)                                                                            # batch_size x 1
-
-        return F.log_softmax(pi, dim=1)
+        pi = self.fc3(s)
+        return F.softmax(pi, dim=1).numpy()
 
     def save_params(self, episode: int):
         file_name = self.file_structure + "checkpoint_" + str(episode) + ".h5"
