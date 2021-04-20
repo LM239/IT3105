72d23afaa1f16fc1476a86eef4378992eb0cc5f1
larsmhod <larsmhod@ntnu.no>
2021-04-20 12:37:05 +0200

    car world finished?

--- P3/worlds/car_world.py
+++ P3/worlds/car_world.py
@@ -3,7 +3,8 @@ import random
 
 class CarWorld:
     def __init__(self):
-        self.state = [random.random() - 0.4, 0.0]
+        self.state = [random.random() / 5 - 0.6, 0.0]
+        self.action_count = 0
 
     def get_action(self):
         return [-1, 0, 1]
@@ -17,4 +18,14 @@ class CarWorld:
     def do_action(self, a):
         self.state[1] += 0.001 * a - 0.0025 * math.cos(3 * self.state[0])
         self.state[0] += self.state[1]
+        self.action_count += 1
+
+    def halt_episode(self):
+        return self.action_count >= 1000 or self.state[0] == 0.6
+
+    def reset_state(self):
+        self.state = [random.random() / 5 - 0.6, 0.0]
+        self.action_count = 0
+
+
 
