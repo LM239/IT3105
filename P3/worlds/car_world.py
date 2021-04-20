35da657bcf90d0419a25a60f0dbe068cf11810a3
larsmhod <larsmhod@ntnu.no>
2021-04-20 12:30:12 +0200

    ca world

--- P3/worlds/car_world.py
+++ P3/worlds/car_world.py
@@ -1,4 +1,16 @@
+import math
+import random
 
 class CarWorld:
     def __init__(self):
-        self.state = {"v": 0.0, "x":0.0}
+        self.state = [random.random() - 0.4, 0.0]
+
+    def get_action(self):
+        return [-1, 0, 1]
+
+    def coarse_code(self):
+        pass
+
+    def get_height(self):
+        return math.cos(3 * (self.state[0] + math.pi / 2))
+
