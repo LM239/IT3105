56c5a62e5173272df56e9502af110aee6ef5122f
larsmhod <larsmhod@ntnu.no>
2021-04-20 12:33:01 +0200

    do action

--- P3/worlds/car_world.py
+++ P3/worlds/car_world.py
@@ -14,3 +14,7 @@ class CarWorld:
     def get_height(self):
         return math.cos(3 * (self.state[0] + math.pi / 2))
 
+    def do_action(self, a):
+        self.state[1] += 0.001 * a - 0.0025 * math.cos(3 * self.state[0])
+        self.state[0] += self.state[1]
+
